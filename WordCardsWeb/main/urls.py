a2b80a03199ab9a4acc9be32ff08dbee5b503f05
Vadym Drok <drokvadim@gmail.com>
2021-04-15 09:40:28 +0300

    testing statics file

--- WordCardsWeb/main/urls.py
+++ WordCardsWeb/main/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import WordListView, WordCreateView, WordDetailView
+from .views import (
+    WordListView,
+    WordCreateView,
+    WordDetailView,
+    WordUpdateView,
+    )
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # path('', WordCreateView.as_view(), name='main'),
     path('dictionary/', WordListView.as_view(), name='dictionary'),
-    path('create/', WordCreateView.as_view(), name='word-create'),
-    path('<int:pk>/', WordDetailView.as_view(), name='word-detail'),
+    path('dictionary/create/', WordCreateView.as_view(), name='word-create'),
+    path('dictionary/<int:pk>/', WordDetailView.as_view(), name='word-detail'),
+    path('dictionary/<int:pk>/update/', WordUpdateView.as_view(), name='word-update'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WordCardsWeb/main/views.py
+++ WordCardsWeb/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Word
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .forms import WordModelForm
 
 from django.views.generic import (
@@ -11,15 +11,29 @@ from django.views.generic import (
     DetailView
     )
 
+
 class WordListView(ListView):
     template_name = 'main/dictionary.html'
     queryset = Word.objects.all()
+
+
+class WordUpdateView(UpdateView):
+    model = Word
+    form_class = WordModelForm
+    template_name = 'main/word_create.html'
+    success_url = '/dictionary/'
 
 
 class WordCreateView(CreateView):
     template_name = 'main/word_create.html'
     form_class = WordModelForm
     queryset = Word.objects.all()
+    success_url = '/dictionary/'
+
+
+class WordCreateView(DeleteView):
+    model = Word
+    success_url = reverse_lazy('dictionary')
 
 
 class WordDetailView(DetailView):
